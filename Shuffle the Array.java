279ad3d143d15ff09cc2a588c09993c08de530f1
Codelord <75088235+SovanRoy-ops@users.noreply.github.com>
2021-12-20 14:36:35 +0530

    Update Shuffle the Array.java

--- Shuffle the Array.java
+++ Shuffle the Array.java
@@ -8,3 +8,14 @@ class Solution {
         return arr;
     }
 }
+
+
+// Alternate approach
+
+public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < 2 * n; ++i) {
+            ans[i] = nums[i / 2 + i % 2 * n];
+        }
+        return ans;
+    }
