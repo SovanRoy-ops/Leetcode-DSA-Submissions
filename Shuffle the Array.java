d4577fb3040da5413cb4b2a3b39822c4f4e2adfa
Codelord <75088235+SovanRoy-ops@users.noreply.github.com>
2021-12-20 20:28:45 +0530

    Update Shuffle the Array.java

--- Shuffle the Array.java
+++ Shuffle the Array.java
@@ -18,4 +18,25 @@ public int[] shuffle(int[] nums, int n) {
             ans[i] = nums[i / 2 + i % 2 * n];
         }
         return ans;
+}
+
+
+// Alternate approach using bit manipulation
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+          
+        final int len = nums.length;
+        
+        for(int i = n; i < len; i++){
+            nums[i] = (nums[i] << 10) | nums[i - n];
+        }
+        
+        int index = 0;
+        for(int i = n; i < len; i++, index += 2){
+            nums[index] = nums[i] & 1023;
+            nums[index + 1] = nums[i] >>> 10;
+        }
+        
+        return nums;
     }
+}
