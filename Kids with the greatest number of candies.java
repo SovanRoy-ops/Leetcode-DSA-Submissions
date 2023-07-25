77422533d29ed1b0299d284c09a3e807f0387e83
Codelord <75088235+SovanRoy-ops@users.noreply.github.com>
2023-07-26 01:09:18 +0530

    Update Kids with the greatest number of candies.java

--- Kids with the greatest number of candies.java
+++ Kids with the greatest number of candies.java
@@ -1,3 +1,4 @@
+//1st approach(BEST) TC - O(n), SC - O(n)
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> result=new ArrayList<>();
@@ -10,5 +11,23 @@ class Solution {
             result.add(candy+extraCandies >= max);
         }
         return result;
+    }
+}
+
+//2nd Approach TC - O(n^2), SC - O(n)
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res = new ArrayList<>();
+        for(int i=0;i<candies.length;i++){
+            int newCandies = candies[i]+extraCandies;
+            res.add(true);
+            for(int j=0;j<candies.length;j++){
+                if(newCandies<candies[j]){
+                    res.set(i,false);
+                    break;
+                }
+            }
+        }
+        return res;
     }
 }
