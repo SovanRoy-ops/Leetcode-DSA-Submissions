c27b36dfd4e3bc1e80e6402ce13c551a939be5e4
Codelord <75088235+SovanRoy-ops@users.noreply.github.com>
2023-08-17 21:09:27 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 64, 4, 52, 31, 10};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = 0; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
