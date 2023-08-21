30f850d7764614241fbd821196b52d2afda4650b
Codelord <75088235+SovanRoy-ops@users.noreply.github.com>
2023-08-21 23:04:05 +0530

    Create CyclicSort.java

---
+++ CyclicSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 1, 4, 2};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
